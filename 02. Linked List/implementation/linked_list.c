e36c2a232d491ea98294226252161f0027359239
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-09-14 13:11:16 -0300

    learning: learning linked list

--- 02. Linked List/implementation/linked_list.c
+++ 02. Linked List/implementation/linked_list.c
@@ -1,4 +1,56 @@
+#include <stdlib.h>
+#include "linked_list.h"
+
 int main(void)
 {
+    printf("Creating a linked list...\n");
+
+    struct Node *linked_list;
+
+    linked_list = (struct Node *)malloc(sizeof(struct Node));
+    linked_list->item = 0;
+    linked_list->next = NULL;
+
+    print_linked_list(&linked_list);
+
+    printf("\nLinked list created!\n");
+
+    printf("\n************************\n");
+
+    printf("\nAdding an item before node...\n");
+
+    add_before_node(5, &linked_list);
+    print_linked_list(&linked_list);
+
+    printf("\nItem added!\n");
+
+    printf("\n************************\n");
+
+    printf("\nAdding an item after node...\n");
+
+    add_after_node(8, &linked_list);
+    print_linked_list(&linked_list);
+
+    printf("\nItem added!\n");
+
+    printf("\n************************\n");
+
+    printf("\nAdding an item at the end...\n");
+
+    add_at_end(12, &linked_list);
+    print_linked_list(&linked_list);
+
+    printf("\nItem added!\n");
+
+    printf("\n************************\n");
+
+    printf("\nDeleting item at index 2...\n");
+
+    delete_from_linked_list_at(&linked_list, 2);
+    print_linked_list(&linked_list);
+
+    printf("\nItem deleted!\n");
+
+    free(linked_list);
     return 0;
 }
---
+++ 02. Linked List/implementation/linked_list.h
@@ -0,0 +1,105 @@
+#include <stdio.h>
+
+struct Node
+{
+    int item;
+    struct Node *next;
+};
+
+void _go_to_next(struct Node **node_to_advance);
+
+void print_linked_list(struct Node **node_to_iterate)
+{
+    struct Node *current_node = (struct Node *)malloc(sizeof(struct Node));
+    current_node->item = (*node_to_iterate)->item;
+    current_node->next = (*node_to_iterate)->next;
+
+    while (current_node != NULL)
+    {
+        printf("%i -> ", current_node->item);
+        _go_to_next(&current_node);
+    }
+}
+
+void _go_to_next(struct Node **node_to_advance)
+{
+    int is_node_null = (*node_to_advance) == NULL;
+    int is_next_node_null = (*node_to_advance)->next == NULL;
+
+    if (!is_node_null & !is_next_node_null)
+    {
+        (*node_to_advance)->item = (*node_to_advance)->next->item;
+        (*node_to_advance)->next = (*node_to_advance)->next->next;
+        return;
+    }
+
+    (*node_to_advance) = NULL;
+}
+
+void add_before_node(int item_to_add, struct Node **node_after)
+{
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->item = item_to_add;
+    new_node->next = (*node_after);
+
+    (*node_after) = new_node;
+}
+
+void add_after_node(int item_to_add, struct Node **node_before)
+{
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->item = item_to_add;
+    new_node->next = (*node_before)->next;
+    (*node_before)->next = new_node;
+}
+
+void add_at_end(int item_to_add, struct Node **node_before)
+{
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->item = item_to_add;
+    new_node->next = NULL;
+
+    if (*node_before == NULL)
+    {
+        (*node_before) = new_node;
+        return;
+    }
+
+    struct Node *current_node = *node_before;
+
+    while (current_node->next != NULL)
+    {
+        current_node = current_node->next;
+    }
+
+    current_node->next = new_node;
+}
+
+void delete_from_linked_list_at(struct Node **beginning, int index_to_delete)
+{
+    if (index_to_delete == 0)
+    {
+        *beginning = (*beginning)->next;
+        return;
+    }
+
+    struct Node *current_node = *beginning;
+
+    for (int current_index = 0; current_index < (index_to_delete - 1); current_index++)
+    {
+        if (current_node->next == NULL)
+        {
+            printf("The element to delete was not found");
+            return;
+        }
+        current_node = current_node->next;
+    }
+
+    if (current_node->next->next != NULL)
+    {
+        current_node->next = current_node->next->next;
+        return;
+    }
+
+    current_node->next = NULL;
+}
