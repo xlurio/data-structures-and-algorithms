df93b3ffc07d6a41c4357a810b074f06f5ecf928
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-09-15 17:06:04 -0300

    learning: learning merge sort

--- 11. Merge Sort/src/merge_sort.h
+++ 11. Merge Sort/src/merge_sort.h
@@ -42,23 +42,20 @@ void merge_sort(int **array_to_sort, int first_index, int mid_index, int last_in
 
     int left_item, right_item;
 
-    while (left_index <= mid_index)
+    while (left_index <= mid_index && right_index <= last_index)
     {
-        while (right_index <= last_index)
-        {
-            left_item = (*array_to_sort)[left_index];
-            right_item = (*array_to_sort)[right_index];
+        left_item = (*array_to_sort)[left_index];
+        right_item = (*array_to_sort)[right_index];
 
-            if (left_item < right_item)
-            {
-                result_array[++result_index] = left_item;
-                left_index++;
-            }
-            else
-            {
-                result_array[++result_index] = right_item;
-                right_index++;
-            }
+        if (left_item < right_item)
+        {
+            result_array[++result_index] = left_item;
+            left_index++;
+        }
+        else
+        {
+            result_array[++result_index] = right_item;
+            right_index++;
         }
     }
 
